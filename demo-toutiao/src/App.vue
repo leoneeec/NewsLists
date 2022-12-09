4add884c1a554712792284a29464a74a0b843a3d
leoneeec <ennaccc@gmail.com>
2022-12-09 18:47:14 +0800

    first commit

---
+++ demo-toutiao/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+    <!-- 头部路由组件 -->
+
+    <router-view></router-view>
+
+    <!-- 底部tabbar组件 -->
+    <van-tabbar v-model="active">
+      <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="user-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      active: 0
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
